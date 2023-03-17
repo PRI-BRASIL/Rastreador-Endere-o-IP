5b02890e5e235ad4e0dc4b6ade9d0b183b87f7bd
PRI-BRASIL <priscila.d.brasil2023@gmail.com>
2023-03-16 22:45:17 -0300

    finalizado a geolocation

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+let h2 = document.querySelector('h2');
+
 function success(pos){
-     console.log(pos.coords.latitude);
+     console.log(pos.coords.latitude, pos.coords.longitude);
+     h2.textContent = `Latitude:${pos.coords.latitude}, Longitude:${pos.coords.longitude}`;
 }
-navigator.geolocation.getCurrentPosition(success);
+
+function error(err){
+    console.log(err);
+}
+var watchID = navigator.geolocation.watchPosition(success, error,{
+    enableHighAccuracy: true,
+    timeout:5000
+
+});
+              //navigator.geolocation.clearwatch(watchID);não vai mais monitorar a posição atual
